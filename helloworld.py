015896888d0209cee10fa5055236f424be324058
Allison <swan168049040@gmail.com>
2021-03-31 13:57:56 -0400

    Initial Commit

---
+++ helloworld.py
@@ -0,0 +1,34 @@
+import os
+os.system('clear')
+
+####################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+####################
+
+
+
+first_name = "John"
+age = 41
+
+fave_pizza = { 
+	"John": "Pepperoni",
+	"Bob": "Mushroom",
+	"Mary": "Cheese",
+}
+
+print (fave_pizza["Mary"])
+
+
+
+
+
+
+
+
+
